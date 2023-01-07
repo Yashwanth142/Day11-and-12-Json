c17ce2c8a2139c0b10779b8b3399300e0ae485cb
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2023-01-07 16:47:04 +0530

    Stock management added

---
+++ StockManagement/StockManagement/ReadData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace StockManagement
+{
+    public class ReadData
+    {
+
+        public StockModel Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<StockModel>(json);
+
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
